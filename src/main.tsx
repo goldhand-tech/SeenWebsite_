82c2d82fc6c7dc8acdde39edb011a0a94d9f6689
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-08-24 10:05:30 +0200

    delpoy

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import "./index.css";
 import "./App.css";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./router/router.tsx";
-// import { QueryClient, QueryClientProvider } from "react-query";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App.tsx";
-// const queryClient = new QueryClient({});
+const queryClient = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <QueryClientProvider client={queryClient}>
-  //   <RouterProvider router={router} />
-  // </QueryClientProvider>
-  <App></App>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+  // <App></App>
 );
