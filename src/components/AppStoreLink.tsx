63763b6dfe6ad98178fe7613c809a08233f91131
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-09-04 12:31:20 +0200

    Link to app store deploy

--- src/components/AppStoreLink.tsx
+++ src/components/AppStoreLink.tsx
@@ -1,5 +1,6 @@
 export const AppStoreLink = () => {
-  window.location.href = "https://google.com";
+  window.location.href =
+    "https://apps.apple.com/us/app/catchballcatch/id6449621772";
   return (
     <>
       <h1 className="redirect">This will redirect you to the App Store</h1>
