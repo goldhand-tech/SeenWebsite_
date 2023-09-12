9591d2395c8d3e339a6a20a7e9136622378bc435
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-09-12 08:56:35 +0200

    website changed

--- src/components/AppStoreLink.tsx
+++ src/components/AppStoreLink.tsx
@@ -1,6 +1,5 @@
 export const AppStoreLink = () => {
-  window.location.href =
-    "https://apps.apple.com/us/app/catchballcatch/id6449621772";
+  window.location.href = "https://apps.apple.com/us/app/seen/id6463472387";
   return (
     <>
       <h1 className="redirect">This will redirect you to the App Store</h1>
